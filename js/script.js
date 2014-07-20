537cbbd523d1e96ecc6993ef50bd02b44e84d966
André Neubauer <andre.neubauer@googlemail.com>
2014-07-20 12:21:01 +0200

    Fixing wrong selector for scrolling

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ jQuery(document).ready(function ($) {
 	//Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
 	//easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
 	function goToByScroll(dataslide) {
-		var offset_top = ( dataslide == 1 ) ? '0px' : $('.slide[data-slide="' + dataslide + '"]').offset().top;
+		var offset_top = $('#slide-' + dataslide).offset().top;
 		
 		htmlbody.stop(false, false).animate({
 			scrollTop: offset_top
